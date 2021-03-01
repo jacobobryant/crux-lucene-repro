9424ec4d52eac73d5f823c10463f01e48c5defdd
Jacob O'Bryant <git@jacobobryant.com>
2021-03-01 09:28:58 -0800

    second commit

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
+if ! which java; then
+  apt-get -y update
+  apt-get -y install default-jre
+  curl -O https://download.clojure.org/install/linux-install-1.10.2.796.sh
+  chmod +x linux-install-1.10.2.796.sh
+  sudo ./linux-install-1.10.2.796.sh
+fi
+
 rm -rf .cpcache data
-clj -M -m foo
+clojure -M -m foo
 rm -r data/{index,lucene}
-clj -M -m foo
+clojure -M -m foo
